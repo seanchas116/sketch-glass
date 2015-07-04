82c55d701efefedb95420009a4847b7d849d693e
Ryohei Ikegami <iofg2100@gmail.com>
2015-07-04 22:03:39 +0900

    Do not ignore /typings

---
+++ typings/bundle.d.ts
@@ -0,0 +1,5 @@
+/// <reference path="node/node.d.ts" />
+/// <reference path="rx/rx.d.ts" />
+/// <reference path="rx/rx-lite.d.ts" />
+/// <reference path="lodash/lodash.d.ts" />
+/// <reference path="touch-events/touch-events.d.ts" />
