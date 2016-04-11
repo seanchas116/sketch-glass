355ff0f4701b0e482f80704923dd6360d792412e
Ryohei Ikegami <iofg2100@gmail.com>
2016-04-11 15:13:09 +0900

    Add Variable for rx

---
+++ src/lib/rx/Variable.ts
@@ -0,0 +1,26 @@
+import * as Rx from "rx";
+
+export default
+class Variable<T> {
+  private _value: T;
+  private _subject: Rx.BehaviorSubject<T>;
+
+  get value() {
+    return this._value;
+  }
+  set value(newValue: T) {
+    if (this._value != newValue) {
+      this._value = newValue;
+      this._subject.onNext(newValue);
+    }
+  }
+
+  get observable() {
+    return this._subject;
+  }
+
+  constructor(value: T) {
+    this._value = value;
+    this._subject = new Rx.BehaviorSubject(value);
+  }
+}
