898c8209f41500cd49189ceaa2aa8a5525398018
Ryohei Ikegami <iofg2100@gmail.com>
2016-04-14 18:27:40 +0900

    Slide sidebar

--- src/view/SideBarView.tsx
+++ src/view/SideBarView.tsx
@@ -1,19 +1,34 @@
 import * as React from "react";
 import ButtonView from "./ButtonView";
+const classNames = require("classnames");
+
+interface SideBarState {
+  open: boolean;
+}
 
 export default
-class SideBarView extends React.Component<void, void> {
+class SideBarView extends React.Component<void, SideBarState> {
+  state = {open: true}
+
   render() {
     return (
-      <aside className="sg-sidebar">
-        <input placeholder="Search" className="sg-search" />
-        <div className="sg-canvas-cell">
-          <div className="thumbnail"></div>
-          <p className="title">Design sketch</p>
-          <p className="updated-at">2 days ago</p>
+      <div className={classNames("sg-sidebar", {open: this.state.open})}>
+        <div className="sg-sidebar-clip">
+          <aside className="sg-sidebar-content">
+            <input placeholder="Search" className="sg-search" />
+            <div className="sg-canvas-cell">
+              <div className="thumbnail"></div>
+              <p className="title">Design sketch</p>
+              <p className="updated-at">2 days ago</p>
+            </div>
+          </aside>
         </div>
-        <ButtonView kind="sidebar" />
-      </aside>
+        <ButtonView kind="sidebar" checked={this.state.open} onClick={() => this.toggleOpen()}/>
+      </div>
     );
+  }
+
+  toggleOpen() {
+    this.setState({open: !this.state.open});
   }
 }
