dbd3130ce93f035cdb0b9115ca566a6ff3fe896b
Ryohei Ikegami <iofg2100@gmail.com>
2016-04-14 16:02:23 +0900

    Install React

--- typings/browser.d.ts
+++ typings/browser.d.ts
@@ -1,3 +1,4 @@
 /// <reference path="browser/ambient/lodash/index.d.ts" />
 /// <reference path="browser/ambient/node/index.d.ts" />
+/// <reference path="browser/ambient/react/index.d.ts" />
 /// <reference path="browser/ambient/rx/index.d.ts" />
--- typings/main.d.ts
+++ typings/main.d.ts
@@ -1,3 +1,4 @@
 /// <reference path="main/ambient/lodash/index.d.ts" />
 /// <reference path="main/ambient/node/index.d.ts" />
+/// <reference path="main/ambient/react/index.d.ts" />
 /// <reference path="main/ambient/rx/index.d.ts" />
