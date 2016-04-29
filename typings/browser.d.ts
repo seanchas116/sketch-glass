804bdbd514fd7ebc36322c6e72dbc2c630986de1
Ryohei Ikegami <iofg2100@gmail.com>
2016-04-29 22:22:17 +0900

    Remove React typings

--- typings/browser.d.ts
+++ typings/browser.d.ts
@@ -2,7 +2,5 @@
 /// <reference path="browser/ambient/lodash/index.d.ts" />
 /// <reference path="browser/ambient/node/index.d.ts" />
 /// <reference path="browser/ambient/page/index.d.ts" />
-/// <reference path="browser/ambient/react-dom/index.d.ts" />
-/// <reference path="browser/ambient/react/index.d.ts" />
 /// <reference path="browser/ambient/rx/index.d.ts" />
 /// <reference path="browser/ambient/whatwg-fetch/index.d.ts" />
--- typings/main.d.ts
+++ typings/main.d.ts
@@ -2,7 +2,5 @@
 /// <reference path="main/ambient/lodash/index.d.ts" />
 /// <reference path="main/ambient/node/index.d.ts" />
 /// <reference path="main/ambient/page/index.d.ts" />
-/// <reference path="main/ambient/react-dom/index.d.ts" />
-/// <reference path="main/ambient/react/index.d.ts" />
 /// <reference path="main/ambient/rx/index.d.ts" />
 /// <reference path="main/ambient/whatwg-fetch/index.d.ts" />
