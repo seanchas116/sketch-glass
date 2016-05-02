190513a4cb3d0b2dc33086a0a60897f6f9d66662
Ryohei Ikegami <iofg2100@gmail.com>
2016-05-02 13:05:21 +0900

    Remove unused files

--- src/Dispatcher.ts
+++
@@ -1,4 +0,0 @@
-import * as Rx from "rx";
-
-const Dispatcher = new Rx.Subject<any>();
-export default Dispatcher;
--- src/util.ts
+++
@@ -1,6 +0,0 @@
-import * as crypto from "crypto";
-
-export
-function md5(text: string) {
-  return crypto.createHash('sha1').update(text).digest('base64');
-}
