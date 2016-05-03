68cac4319092bafc089a7abdaf7a564719cb0043
Ryohei Ikegami <iofg2100@gmail.com>
2016-05-03 11:04:55 +0900

    Generate ID for Stroke

--- src/model/Stroke.ts
+++ src/model/Stroke.ts
@@ -1,5 +1,6 @@
 import Vec2 from '../lib/geometry/Vec2';
 import Color from '../lib/geometry/Color';
+import randomID from "../lib/randomID";
 
 export
 interface StrokeData {
@@ -7,18 +8,20 @@ interface StrokeData {
   points: [number, number][];
   color: [number, number, number, number];
   width: number;
+  id: string;
 }
 
 export default
 class Stroke {
-  constructor(public points: Vec2[], public color: Color, public width: number) {
+  constructor(public points: Vec2[], public color: Color, public width: number, public id = randomID()) {
   }
 
   static fromData(data: StrokeData) {
     return new Stroke(
       data.points.map(([x, y]) => new Vec2(x, y)),
       new Color(data.color[0], data.color[1], data.color[2], data.color[3]),
-      data.width
+      data.width,
+      data.id
     );
   }
 
@@ -28,6 +31,7 @@ class Stroke {
       points: this.points.map(p => [p.x, p.y] as [number, number]),
       color: [this.color.r, this.color.g, this.color.b, this.color.a],
       width: this.width,
+      id: this.id
     };
   }
 }
