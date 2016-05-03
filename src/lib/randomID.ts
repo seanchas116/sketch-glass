08ecf755c82d77482bdb4e740273a62aeaf6b816
Ryohei Ikegami <iofg2100@gmail.com>
2016-05-03 11:04:17 +0900

    Use base64url as random ID

--- src/lib/randomID.ts
+++ src/lib/randomID.ts
@@ -1,7 +1,8 @@
 import * as crypto from "crypto";
+const base64url = require("base64url");
 
-// generates a random ID that can be used as Firebase key
+// generates a random ID based on base64url
 export default
-function randomID() {
-  return crypto.randomBytes(15).toString("base64").replace("/", "-");
+function randomID(): string {
+  return base64url(crypto.randomBytes(15));
 }
