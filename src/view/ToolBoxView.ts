e7bcdc2164dea64e829f90e3d2d01454b6ae9d60
Ryohei Ikegami <iofg2100@gmail.com>
2016-05-06 14:43:32 +0900

    Reflect canUndo/canRedo

--- src/view/ToolBoxView.ts
+++ src/view/ToolBoxView.ts
@@ -3,6 +3,7 @@ import ButtonView from "./ButtonView";
 import Tool from "../model/Tool";
 import Variable from "../lib/rx/Variable";
 import ColorButtonView from "./ColorButtonView";
+import DisposableBag from "../lib/DisposableBag";
 import {appViewModel} from "../viewmodel/AppViewModel";
 
 export default
@@ -23,6 +24,7 @@ class ToolBoxView extends Component {
   undoButton = new ButtonView(this.elementFor(".undo-button"), "undo");
   redoButton = new ButtonView(this.elementFor(".redo-button"), "redo");
   buttons = [this.penButton, this.eraserButton, this.undoButton, this.redoButton, this.colorButton];
+  canvasDisposables = new DisposableBag();
 
   constructor(mountPoint: Element) {
     super(mountPoint);
@@ -30,6 +32,7 @@ class ToolBoxView extends Component {
     const viewModel = appViewModel.toolBoxViewModel;
 
     this.disposables.add(
+      this.canvasDisposables,
       ...this.buttons.map(button =>
         appViewModel.isLoading.observable.subscribe(button.isDisabled)
       )
@@ -53,6 +56,15 @@ class ToolBoxView extends Component {
       }),
       this.redoButton.clicked.subscribe(() => {
         this.redo();
+      }),
+      appViewModel.canvasViewModel.observable.subscribe(canvasVM => {
+        this.canvasDisposables.clear();
+        if (canvasVM != undefined) {
+          this.canvasDisposables.add(
+            canvasVM.canvas.canUndo.observable.map(x => !x).subscribe(this.undoButton.isDisabled),
+            canvasVM.canvas.canRedo.observable.map(x => !x).subscribe(this.redoButton.isDisabled)
+          );
+        }
       })
     );
   }
