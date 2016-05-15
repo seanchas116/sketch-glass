dd71b857eae3d0615d0085e94a71aaffeba41a43
Ryohei Ikegami <iofg2100@gmail.com>
2016-05-15 16:19:33 +0900

    Enter/esc to ok/cancel

--- src/view/NewCanvasDialog.ts
+++ src/view/NewCanvasDialog.ts
@@ -6,33 +6,41 @@ import * as Rx from "rx";
 export default
 class NewCanvasDialog extends Component {
     static template = `
-    <div class='sg-new-canvas-dialog'>
-      <section class='dialog'>
-        <h1>Create New Canvas</h1>
-        <div class="inputs">
-          <input class='name' placeholder='Name...'>
-          <button class='ok'>Create</button>
+        <div class='sg-new-canvas-dialog'>
+            <section class='dialog'>
+                <h1>Create New Canvas</h1>
+                <div class="inputs">
+                    <input class='name' placeholder='Name...'>
+                    <button class='ok'>Create</button>
+                </div>
+            </section>
         </div>
-      </section>
-    </div>
-  `;
+    `;
 
-    clicked = this.slotFor(".ok").clicked;
+    nameSlot = this.slotFor<HTMLInputElement>(".name");
+    okSlot = this.slotFor(".ok");
     isCancellable = true;
     backgroundClicked = this.slot.clicked;
 
     constructor(mountPoint: MountPoint) {
         super(mountPoint);
         this.elementFor(".dialog").addEventListener("click", e => e.stopPropagation());
-        this.subscribe(this.clicked, async () => {
-            const name = (this.elementFor(".name") as HTMLInputElement).value;
-            await appViewModel.addFile(name);
+        this.subscribe(this.okSlot.clicked, () => this.ok());
+        this.subscribe(this.backgroundClicked, () => this.cancel());
+        this.subscribe(this.nameSlot.enterPressed, () => this.ok());
+        this.subscribe(this.nameSlot.escPressed, () => this.cancel());
+        this.nameSlot.element.focus();
+    }
+
+    async ok() {
+        const name = this.nameSlot.element.value;
+        await appViewModel.addFile(name);
+        this.dispose();
+    }
+
+    cancel() {
+        if (this.isCancellable) {
             this.dispose();
-        });
-        this.subscribe(this.backgroundClicked, () => {
-            if (this.isCancellable) {
-                this.dispose();
-            }
-        });
+        }
     }
 }
